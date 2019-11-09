a7ff218b185b2b844d6df120a22671708b51f466
matteo.zh <s223012@studenti.polito.it>
2019-11-09 22:58:25 +0100

    Update step1.py

--- lab1/step1.py
+++ lab1/step1.py
@@ -12,17 +12,27 @@ permanent_parkings = db['PermanentParkings']
 active_bookings = db['ActiveBookings']
 active_parkings = db['ActiveParkings']
 
-print("permanent bookings: ", permanent_bookings.count())
-print("permanent parkings: ", permanent_parkings.count())
-print("active bookings: ", active_bookings.count())
-print("active parkings: ", active_parkings.count())
+print("permanent bookings: ", permanent_bookings.estimated_document_count())
+print("permanent parkings: ", permanent_parkings.estimated_document_count())
+print("active bookings: ", active_bookings.estimated_document_count())
+print("active parkings: ", active_parkings.estimated_document_count())
 
 # permanent booking and parking similar because of tot n of cars ideally one car should be booked then parked.
 
 print("distinct cities: ", permanent_bookings.distinct('city'))
-print("start end time: ", )
 
-seattle = permanent_bookings.find({}, {'city': 'Seattle'})
+start_unix = permanent_bookings.find().sort('init_time', 1).limit(1)
+end_unix = permanent_bookings.find().sort('init_time', -1).limit(1)
+
+for i in start_unix:
+    start = i['init_time']
+
+for i in end_unix:
+    end = i['init_time']
+
+print("start: {} end: {}".format(start, end))  # Tuesday 13 December 2016 17:38:23 Wednesday 31 January 2018 13:11:33
+
+
 
 
 
