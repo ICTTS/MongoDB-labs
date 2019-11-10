c84d306a71088de7d825aeb1b25d2870d860a645
Francesca Fasano <franci.fasano@gmail.com>
2019-11-10 12:01:54 +0100

    Add city iteration

--- lab1/step2_1.py
+++ lab1/step2_1.py
@@ -21,69 +21,79 @@ db.authenticate('ictts', 'Ictts16!')#, mechanism='MONGODB-CR') #authentication
 PermanentBookings = db[COLLECTION] # Collection for Car2go to use
 
 start = "01/10/2017"
-start_time = time.mktime(datetime.datetime.strptime(start, "%d/%m/%Y").timetuple())
 end = "01/11/2017"
+start_time = time.mktime(datetime.datetime.strptime(start, "%d/%m/%Y").timetuple())
 end_time = time.mktime(datetime.datetime.strptime(end, "%d/%m/%Y").timetuple())
+start_time_seattle = start_time -10*60*60
+end_time_seattle = end_time -9*60*60
 
-city = "Wien"
-duration_list = []
-num_of_documents = 0
-count = 0
+city_list = ["Wien", "Torino", "Seattle"]
 
-# Pipeline:
-# Select only documents with city=Torino in October 2017, calculate duration,
-# sort by duration in ascending order
-my_collection = list(PermanentBookings.aggregate([
-    {'$match':{
-        '$and':[
-        {'city': city},
-        {'init_time': {'$gt': start_time, '$lt': end_time}}]
+for city in city_list:
+    duration_list = []
+    num_of_documents = 0
+    count = 0
+    
+    if (city == "Seattle"):
+        start_time = start_time_seattle
+        end_time = end_time_seattle
+        
+    # Pipeline:
+    # Select only documents with city=Torino in October 2017, calculate duration,
+    # sort by duration in ascending order
+    my_collection = list(PermanentBookings.aggregate([
+        {'$match':{
+            '$and':[
+            {'city': city},
+            {'init_time': {'$gt': start_time, '$lt': end_time}}]
+            }
+        },
+        {'$project':{
+            '_id':0,
+            'duration': {'$subtract': ['$final_time','$init_time']}
+            }
+        },
+        {'$sort':{
+            'duration': 1
         }
-    },
-    {'$project':{
-        '_id':0,
-        'duration': {'$subtract': ['$final_time','$init_time']}
-        }
-    },
-    {'$sort':{
-        'duration': 1
-    }
-    }]))
-
-num_of_documents = len(my_collection) # Number of documents
-
-# Points for grouping in CDF
-starting_point = 0
-how_many = 0
-cnt = 0
-
-# Calculate CDF
-while True:
-    count += 60
-
-    for i in range (starting_point, num_of_documents):
-
-        if my_collection[i]['duration'] < count:
-            how_many += 1
-
-        else:
-            starting_point = i
+        }]))
+    
+    num_of_documents = len(my_collection) # Number of documents
+    
+    # Points for grouping in CDF
+    starting_point = 0
+    how_many = 0
+    cnt = 0
+    
+    # Calculate CDF
+    while True:
+        count += 60
+    
+        for i in range (starting_point, num_of_documents):
+    
+            if my_collection[i]['duration'] < count:
+                how_many += 1
+    
+            else:
+                starting_point = i
+                break
+    
+        duration_list.append(how_many)
+        cnt += 1
+    
+        if duration_list[-1] == num_of_documents:
             break
-
-    duration_list.append(how_many)
-    cnt += 1
-
-    if duration_list[-1] == num_of_documents:
-        break
-
-results = [x/num_of_documents for x in duration_list]
-
-#%% Plots
-plt.semilogx(range(len(duration_list)),results)
+    
+    results = [x/num_of_documents for x in duration_list]
+    
+    #%% Plots
+    plt.semilogx(range(len(duration_list)),results)
+    
 #plt.plot(results)
 plt.xlabel('Minutes')
 plt.ylabel('CDF')
 plt.ylim([0, 1])
+plt.legend(['Turin', 'Wien', 'Seattle'])
 plt.show()
 
 #while True:
