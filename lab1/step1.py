bee3e3dc0fa7f3cba4d35a9163a1299b2cefe5f8
paolo-26 <paolo.1995@ymail.com>
2019-11-10 10:48:48 +0100

    Add available cars
    Answered: How many cars are available?

--- lab1/step1.py
+++ lab1/step1.py
@@ -3,6 +3,7 @@
 import pymongo as pm
 from pprint import pprint
 import pandas as pd
+import datetime
 
 client = pm.MongoClient('bigdatadb.polito.it', ssl=True,
                         authSource='carsharing',
@@ -16,6 +17,7 @@ permanent_parkings = db['PermanentParkings']
 active_bookings = db['ActiveBookings']
 active_parkings = db['ActiveParkings']
 
+print("- How many documents are present in each collection?")
 print("permanent bookings: ", permanent_bookings.estimated_document_count())
 print("permanent parkings: ", permanent_parkings.estimated_document_count())
 print("active bookings: ", active_bookings.estimated_document_count())
@@ -23,8 +25,8 @@ print("active parkings: ", active_parkings.estimated_document_count())
 
 # Permanent booking and parking similar because of tot n of cars ideally one
 # car should be booked then parked.
-
-print("distinct cities: ", permanent_bookings.distinct('city'))
+print("\n- For which cities the system is collecting data?")
+print(permanent_bookings.distinct('city'))
 
 start_unix = permanent_bookings.find().sort('init_time', 1).limit(1)
 end_unix = permanent_bookings.find().sort('init_time', -1).limit(1)
@@ -35,4 +37,18 @@ for i in start_unix:
 for i in end_unix:
     end = i['init_time']
 
-print("start: {} end: {}".format(start, end))  # Tuesday 13 December 2016 17:38:23 Wednesday 31 January 2018 13:11:33
+print("\n- When the collection started? When the collection ended?")
+print("start:", datetime.datetime.utcfromtimestamp(start).strftime('%d-%m-%Y '
+                                                                   '%H:%M:%S'))
+print("end  :", datetime.datetime.utcfromtimestamp(end).strftime('%d-%m-%Y '
+                                                                 '%H:%M:%S'))
+
+print("\n- How many cars are available?")
+print(active_parkings.count_documents({"city": "Torino"}))
+print(active_parkings.count_documents({"city": "Wien"}))
+print(active_parkings.count_documents({"city": "Seattle"}))
+
+print("\n- How many cars are available? (distinct plate)")
+print(len(permanent_parkings.distinct("plate", {"city": "Torino"})))
+print(len(permanent_parkings.distinct("plate", {"city": "Wien"})))
+print(len(permanent_parkings.distinct("plate", {"city": "Seattle"})))
