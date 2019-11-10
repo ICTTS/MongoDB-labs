3771ce03b9916e8f10394f0b491edc57856a3ec9
paolo-26 <paolo.1995@ymail.com>
2019-11-10 10:15:48 +0100

    Add shebang
    Add #!/usr/bin/env python3 and update comments.

--- lab1/step1.py
+++ lab1/step1.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import pymongo as pm
 from pprint import pprint
 import pandas as pd
 
-client = pm.MongoClient('bigdatadb.polito.it', ssl=True, authSource='carsharing', tlsAllowInvalidCertificates=True)
+client = pm.MongoClient('bigdatadb.polito.it', ssl=True,
+                        authSource='carsharing',
+                        tlsAllowInvalidCertificates=True)
 
 db = client["carsharing"]
 db.authenticate('ictts', 'Ictts16!')
@@ -17,7 +21,8 @@ print("permanent parkings: ", permanent_parkings.estimated_document_count())
 print("active bookings: ", active_bookings.estimated_document_count())
 print("active parkings: ", active_parkings.estimated_document_count())
 
-# permanent booking and parking similar because of tot n of cars ideally one car should be booked then parked.
+# Permanent booking and parking similar because of tot n of cars ideally one
+# car should be booked then parked.
 
 print("distinct cities: ", permanent_bookings.distinct('city'))
 
@@ -31,8 +36,3 @@ for i in end_unix:
     end = i['init_time']
 
 print("start: {} end: {}".format(start, end))  # Tuesday 13 December 2016 17:38:23 Wednesday 31 January 2018 13:11:33
-
-
-
-
-
