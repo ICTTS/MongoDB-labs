b4b514f121c556543d6c6dceb157585d3d7a3233
matteo.zh <s223012@studenti.polito.it>
2019-11-11 14:43:45 +0100

    Update step2_6.py

--- lab1/step2_6.py
+++ lab1/step2_6.py
@@ -33,13 +33,17 @@ def main():
     for i in my_collection:
         data.append([i['loc']['coordinates'][0], i['loc']['coordinates'][1], i['hour_of_day']])
 
-    
     df = pd.DataFrame(data, columns=['Latitude', 'Longitude', 'Hour'])
 
     # TODO partizione mattino pomeriggio sera notte
-    intervallo = 'mattino'
-    filename = 'heatmap'+intervallo+'.csv'
-    df.to_csv(filename, mode='w')
+    df_6_12 = df[df['Hour'] >= 6 and df['Hour'] < 12]
+    df_12_18 = df[df['Hour'] >= 12 and df['Hour'] < 18]
+    df_18_24 = df[df['Hour'] >= 18 and df['Hour'] < 24]
+    df_0_6 = df[df['Hour'] >= 0 and df['Hour'] < 6]
+    df_6_12.to_csv('mattino.csv', mode='w')
+    df_12_18.to_csv('pomeriggio.csv', mode='w')
+    df_18_24.to_csv('sera.csv', mode='w')
+    df_0_6.to_csv('notte.csv', mode='w')
 
 if __name__ == '__main__':
     main()
