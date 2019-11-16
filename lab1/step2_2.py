45b1c62f7539ca9f567e3355a90c4da9f275077f
paolo-26 <paolo.1995@ymail.com>
2019-11-16 18:07:08 +0100

    Update style and docstring

--- lab1/step2_2.py
+++ lab1/step2_2.py
@@ -2,37 +2,45 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov  9 10:53:06 2019
+
 Step 2.2
+
+Consider the system utilization over time: aggregate rentals per hour of the
+day, and then plot the number of booked/parked cars (or percentage of
+booked/parked cars) per hour versus time of day.
 """
-import pymongo as pm #import MongoClient only
+import pymongo as pm
 import matplotlib.pyplot as plt
 import time
 import datetime
 
-COLLECTION = 'PermanentBookings' # Name of the collection
+COLLECTION = 'PermanentBookings'
 CITY_LIST = ["Torino", "Wien", "Seattle"]
 
 
 def get_collection():
-    """Connection to database."""
+    """Connect to database and return the collection."""
     client = pm.MongoClient('bigdatadb.polito.it',
                             ssl=True,
                             authSource='carsharing',
                             tlsAllowInvalidCertificates=True)
-    db = client['carsharing'] #Choose the DB to use
-    db.authenticate('ictts', 'Ictts16!')#, mechanism='MONGODB-CR') #authentication
+    db = client['carsharing']
+    db.authenticate('ictts', 'Ictts16!')
     collection = db[COLLECTION]
     return collection
 
 
 def hours_aggregate():
-    """Aggregation per hours of the day."""
+    """Aggregate per hours of the day.
+    start_time and end_time define the interval day period in which data has
+    to be selected (match). Data are grouped by hour_of_day and then sorted.
+    """
     collection = get_collection()
     start = "01/10/2017"
     start_time = time.mktime(datetime.datetime.strptime(start,
                              "%d/%m/%Y").timetuple())
     day_duration = 24*60*60
-    start_time_seattle = start_time -10*60*60
+    start_time_seattle = start_time - 10*60*60
     end_time_seattle = start_time_seattle + day_duration
 
     days = list(range(31))
@@ -42,38 +50,39 @@ def hours_aggregate():
         number_of_rentals = []
         time1 = start_time
         time2 = start_time + day_duration
+
         for day in days:
+            if (city == "Seattle"):
+                time1 = start_time_seattle
+                time2 = end_time_seattle
 
-           if (city == "Seattle"):
-               time1 = start_time_seattle
-               time2 = end_time_seattle
-
-           my_collection = list(collection.aggregate([
-                   {'$match':{
-                       '$and':[
-                       {'city': city},
-                       {'init_time': {'$gte': time1, '$lt': time2}}]}
+            my_collection = list(collection.aggregate([
+                   {'$match': {
+                       '$and': [
+                           {'city': city},
+                           {'init_time': {'$gte': time1, '$lt': time2}}]
+                   }
                    },
-                   {'$project':{
+                   {'$project': {
                        '_id': 0,
                        'hour_of_day': {'$hour': '$init_date'}
-                       }
+                   }
                    },
-                   {'$group':{
+                   {'$group': {
                        '_id': "$hour_of_day",
                        'total': {"$sum": 1}
                    }},
-                   {'$sort':{
-                           '_id': 1
-                           }}
+                   {'$sort': {
+                       '_id': 1
+                   }
+                   }
+            ]))
 
-           ]))
+            time1 = time2
+            time2 = time2 + day_duration
 
-           time1 = time2
-           time2 = time2 + day_duration
-
-           for el in my_collection:
-             number_of_rentals.append(el['total'])
+            for el in my_collection:
+                number_of_rentals.append(el['total'])
 
         plt.plot(number_of_rentals)
 
