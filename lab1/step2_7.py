6331086ee511f6f2c732d29e4f7a231a0c49a580
paolo-26 <paolo.1995@ymail.com>
2019-11-16 13:26:30 +0100

    Update style

--- lab1/step2_7.py
+++ lab1/step2_7.py
@@ -14,12 +14,13 @@ import numpy as np
 COLLECTION = 'PermanentBookings'
 CITY = 'Torino'
 
-#rentals
+# Rentals
 low_limit = 3*60  # Two minutes
-high_limit= 3*60*60  # Three hours
+high_limit = 3*60*60  # Three hours
 
-#pt
-high_limit_pt = 5*60*60 # cosa ha senso? ci sono numeri altissimi nell'elenco, errori?
+# Pt
+# Cosa ha senso? Ci sono numeri altissimi nell'elenco, errori?
+high_limit_pt = 5*60*60
 
 
 def get_collection():
@@ -28,8 +29,8 @@ def get_collection():
                             ssl=True,
                             authSource='carsharing',
                             tlsAllowInvalidCertificates=True)
-    db = client['carsharing'] #Choose the DB to use
-    db.authenticate('ictts', 'Ictts16!')#, mechanism='MONGODB-CR') #authentication
+    db = client['carsharing']  # Choose the DB to use
+    db.authenticate('ictts', 'Ictts16!')
     collection = db[COLLECTION]
     return collection
 
@@ -45,64 +46,70 @@ def pt_duration():
     collection = get_collection()
 
     my_collection = list(collection.aggregate([
-                    {'$match':{
-                        '$and':[
-                        {'city': CITY},
-                        {'public_transport.duration': {'$ne': -1}},
-                        {'walking.duration': {'$ne': -1}},
-                        {'init_time': {'$gte': start_time, '$lt': end_time}}]
-                        }
+                    {'$match': {
+                        '$and': [
+                            {'city': CITY},
+                            {'public_transport.duration': {'$ne': -1}},
+                            {'walking.duration': {'$ne': -1}},
+                            {'init_time': {'$gte': start_time,
+                                           '$lt': end_time}}]
+                    }
                     },
-                    {'$project':{
-                        '_id':0,
-                        'duration': {'$subtract': ['$final_time','$init_time']},
+                    {'$project': {
+                        '_id': 0,
+                        'duration': {'$subtract': ['$final_time',
+                                                   '$init_time']},
                         'pt_duration': '$public_transport.duration',
                         'w_duration': '$walking.duration',
                         'moved': {'$ne': [
-                            {'$arrayElemAt': ['$origin_destination.coordinates',
-                                                  0]},
-                            {'$arrayElemAt': ['$origin_destination.coordinates',
-                                                  1]}]}}
+                            {'$arrayElemAt':
+                             ['$origin_destination.coordinates', 0]},
+                            {'$arrayElemAt':
+                             ['$origin_destination.coordinates', 1]}]}
+                    }
                     },
                     {'$match':
                         {
-                        'moved': True,
-                        'duration': {'$gt': low_limit, '$lt': high_limit},
-                        'pt_duration': {'$lt': high_limit_pt},
-                        'w_duration': {'$lt': high_limit_pt}
-                    }},
-                    {'$project':
-                        {'pt_duration': 1,
-                        'w_duration': 1}
-                    },
-                    {'$sort':{
+                            'moved': True,
+                            'duration': {'$gt': low_limit, '$lt': high_limit},
+                            'pt_duration': {'$lt': high_limit_pt},
+                            'w_duration': {'$lt': high_limit_pt}
+                        }},
+                    {'$project': {
                         'pt_duration': 1,
-                        'w_duration':1
+                        'w_duration': 1
+                    }
+                    },
+                    {'$sort': {
+                        'pt_duration': 1,
+                        'w_duration': 1
                     }
                     },
                     {'$group': {
                         '_id': 'null',
-                        'pt_array': {'$push' : '$pt_duration'},
-                        'w_array': {'$push' : '$w_duration'}}
-                        },
-                        {'$project': {
-                            '_id' : 0,
-                            'pt_array': 1,
-                            'w_array': 1}
-                            }]))
+                        'pt_array': {'$push': '$pt_duration'},
+                        'w_array': {'$push': '$w_duration'}
+                    }
+                    },
+                    {'$project': {
+                        '_id': 0,
+                        'pt_array': 1,
+                        'w_array': 1
+                    }
+                    }]))
 
     return (my_collection[0]['pt_array'], my_collection[0]['w_array'])
 
 
 def make_hist(array, bins, color, titled):
-    # print(pt_duration_array[-1]) mmmmm c'è una piccola coda di ouliers che non
-    # si vede a 228 circa neanche mettendo il bin, magari farli comunque?
+    # print(pt_duration_array[-1]) mmmmm c'è una piccola coda di ouliers che
+    # non si vede a 228 circa neanche mettendo il bin, magari farli comunque?
     fig, ax = plt.subplots(constrained_layout=False, figsize=(15, 8))
     plt.hist(np.array(array), bins, ec='black', color=color)
     plt.xlabel(titled + ' duration (minutes)')
     plt.ylabel('No. rentals')
     plt.title('No. rentals given the alternative ' + titled + ' trip'
-               ' duration')
+              ' duration')
     plt.xticks(ticks=bins, labels=bins)
 
 
@@ -112,8 +119,10 @@ def main():
     w_duration_array = [x/60 for x in w]
     blue = (0, 0.4470, 0.7410)
     orange = (0.8500, 0.3250, 0.0980)
-    make_hist(pt_duration_array, list(range(0, 105, 5)), blue, 'public transport')
-    make_hist(w_duration_array, list(range(0, 155, 5)), orange, 'walking')
+    make_hist(pt_duration_array, list(range(0, 105, 5)), blue,
+              'public transport')
+    make_hist(w_duration_array, list(range(0, 155, 5)), orange,
+              'walking')
     plt.show()
 
 
