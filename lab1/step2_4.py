fba2936acece166198bcb6a611efd148dca28edf
paolo-26 <paolo.1995@ymail.com>
2019-11-23 17:47:21 +0100

    Add PermanentParkings

--- lab1/step2_4.py
+++ lab1/step2_4.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 import time
 import datetime
 
-COLLECTION = 'PermanentBookings'
+COLLECTION = ['PermanentBookings', 'PermanentParkings']
 CITY_LIST = ["Torino", "Wien", "Seattle"]
 
 
-def get_collection():
+def get_collection(coll_name):
     """Connect to database."""
     client = pm.MongoClient('bigdatadb.polito.it',
                             ssl=True,
@@ -21,13 +21,12 @@ def get_collection():
                             tlsAllowInvalidCertificates=True)
     db = client['carsharing']
     db.authenticate('ictts', 'Ictts16!')
-    collection = db[COLLECTION]
+    collection = db[coll_name]
     return collection
 
 
 def hours_aggregate():
     """Aggregate per hours of the day."""
-    collection = get_collection()
     start = "01/10/2017"
     start_time = time.mktime(datetime.datetime.strptime(start,
                              "%d/%m/%Y").timetuple())
@@ -36,74 +35,114 @@ def hours_aggregate():
     end_time_seattle = start_time_seattle + day_duration
 
     days = list(range(31))
-    fig, ax = plt.subplots(constrained_layout=False, figsize=(15, 8))
-    low_limit = 2*60  # Two minutes
-    high_limit = 3*60*60  # Three hours
-    for city in CITY_LIST:
-        number_of_rentals = []
-        time1 = start_time
-        time2 = start_time + day_duration
 
-        if (city == "Seattle"):
-            time1 = start_time_seattle
-            time2 = end_time_seattle
+    for coll in COLLECTION:
+        print("Analysing collection %s..." % (coll))
+        collection = get_collection(coll)
+        fig, ax = plt.subplots(constrained_layout=False, figsize=(15, 8))
+        low_limit = 2*60  # Two minutes
+        high_limit = 3*60*60  # Three hours
+        for city in CITY_LIST:
+            number_of_rentals = []
+            time1 = start_time
+            time2 = start_time + day_duration
 
-        for day in days:
-            my_collection = list(collection.aggregate([
-                {'$match': {
-                    '$and': [
-                        {'city': city},
-                        {'init_time': {'$gte': time1, '$lt': time2}}]}
-                 },
-                {'$project': {
-                 '_id': 0,
-                    'hour_of_day': {'$hour': '$init_date'},
-                    'duration': {'$subtract': ['$final_time', '$init_time']},
-                    'moved': {'$ne': [
-                            {'$arrayElemAt':
-                                ['$origin_destination.coordinates', 0]},
-                            {'$arrayElemAt':
-                                ['$origin_destination.coordinates', 1]}]
-                            }
-                 }
-                 },
-                {'$match':
-                    {'moved': True,
-                     'duration': {'$gt': low_limit, '$lt': high_limit}
-                     }
-                 },
-                {'$group': {
-                    '_id': "$hour_of_day",
-                    'total': {"$sum": 1}
-                        }
-                 },
-                {'$sort': {
-                    '_id': 1
-                }}
-                ]))
+            if (city == "Seattle"):
+                time1 = start_time_seattle
+                time2 = end_time_seattle
 
-            time1 = time2
-            time2 = time2 + day_duration
+            for day in days:
+                if coll == 'PermanentBookings':
+                    my_collection = list(collection.aggregate([
+                        {'$match': {
+                            '$and': [
+                                {'city': city},
+                                {'init_time': {'$gte': time1, '$lt': time2}}]}
+                         },
+                        {'$project': {
+                         '_id': 0,
+                            'hour_of_day': {'$hour': '$init_date'},
+                            'duration': {'$subtract': ['$final_time',
+                                                       '$init_time']},
+                            'moved': {'$ne': [
+                                    {'$arrayElemAt':
+                                        ['$origin_destination.coordinates',
+                                         0]},
+                                    {'$arrayElemAt':
+                                        ['$origin_destination.coordinates',
+                                         1]}]
+                                    }
+                         }
+                         },
+                        {'$match':
+                            {'moved': True,
+                             'duration': {'$gt': low_limit, '$lt': high_limit}
+                             }
+                         },
+                        {'$group': {
+                            '_id': "$hour_of_day",
+                            'total': {"$sum": 1}
+                                }
+                         },
+                        {'$sort': {
+                            '_id': 1
+                        }}
+                        ]))
 
-            for el in my_collection:
-                number_of_rentals.append(el['total'])
+                elif coll == 'PermanentParkings':
+                    my_collection = list(collection.aggregate([
+                        {'$match': {
+                            '$and': [
+                                {'city': city},
+                                {'init_time': {'$gte': time1,
+                                               '$lt': time2}}]}
+                         },
+                        {'$project': {
+                         '_id': 0,
+                            'hour_of_day': {'$hour': '$init_date'},
+                            'duration': {'$subtract': ['$final_time',
+                                                       '$init_time']}
+                         }
+                         },
+                        {'$match':
+                            {'duration': {'$gt': low_limit}
+                             }
+                         },
+                        {'$group': {
+                            '_id': "$hour_of_day",
+                            'total': {"$sum": 1}
+                                }
+                         },
+                        {'$sort': {
+                            '_id': 1
+                        }}
+                        ]))
 
-        plt.plot(number_of_rentals)
+                time1 = time2
+                time2 = time2 + day_duration
 
-    plt.xlabel('Hours per day')
-    plt.ylabel('No. of bookings')
-    plt.xticks(ticks=[0, 168, 336, 504, 672],
-               labels=['Oct 1', 'Oct 8', 'Oct 15', 'Oct 22', 'Oct 29'],
-               rotation='horizontal')
-    plt.legend(CITY_LIST, loc=2)
-    plt.grid(which='both')
-    plt.title("Rentals")
-#    plt.savefig('step2.4.eps', format='eps')
+                for el in my_collection:
+                    number_of_rentals.append(el['total'])
+
+            plt.plot(number_of_rentals)
+
+        plt.xlabel('Hours per day')
+        plt.ylabel('No. of bookings')
+        plt.xticks(ticks=[0, 168, 336, 504, 672],
+                   labels=['Oct 1', 'Oct 8', 'Oct 15', 'Oct 22', 'Oct 29'],
+                   rotation='horizontal')
+        plt.legend(CITY_LIST, loc=2)
+        plt.xlim([0, 24*31])
+        plt.grid(which='both')
+        plt.title(coll)
+        plt.savefig('step2_4' + coll + '.eps', format='eps')
+
 
 def main():
     """Call aggregation per hour."""
     hours_aggregate()
     plt.show()
+    print("---END---")
 
 
 if __name__ == '__main__':
