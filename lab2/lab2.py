417c4b2cc0f4a920cb647aa832e07dd6ac53544c
Paolo Grasso <paolo.1995@ymail.com>
2020-01-14 11:28:09 +0100

    Add cycles for different parameters

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+import time
 from sklearn.metrics import (mean_squared_error, mean_absolute_error,
                              r2_score)
-from statsmodels.tsa.stattools import acf, pacf
+# from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 
 
 def read_file(filename):
@@ -23,6 +25,7 @@ def main():
     df = read_file("Torino.csv")
     df = df.rename(columns={'count': 'rental'})
 
+    # ACF
     plt.figure()
     pd.plotting.autocorrelation_plot(df["rental"])
     plt.title('Autocorrelation Function')
@@ -30,16 +33,15 @@ def main():
 
     # ACF
     n_lags = 48
-    acf_ = acf(df["rental"], nlags=n_lags)
-    plt.figure()
-    plt.plot(acf_, 'o-')
+    fig, ax = plt.subplots()
+    plot_acf(df["rental"], ax=ax, lags=n_lags)
     plt.title('Autocorrelation Function - no. lags: %d' % n_lags)
     plt.grid(which='both')
 
     # PACF
-    pacf_ = pacf(df["rental"], nlags=n_lags, method='ols')
-    plt.figure()
-    plt.plot(pacf_, 'o-')
+    n_lags = 48
+    fig, ax = plt.subplots()
+    plot_pacf(df["rental"], ax=ax, lags=n_lags)
     plt.title('Partial Autocorrelation Function - no. lags: %d' % n_lags)
     plt.grid(which='both')
 
@@ -74,24 +76,38 @@ def main():
     print("Starting ARIMA model.""")
     train_size = 24*7*2
     test_size = 96
-    p_list = [0, 1, 2, 3, 4]
-    d = 0
-    q = 1
-    my_arima = run_arima(X, train_size, test_size, p_list, d, q)
-    my_arima.run()
-    my_arima.plot_arima()
+    p_list = [0, 1, 2]
+    q_list = [0, 1, 2]
+    d_list = [0, 1, 2]
+    # grid_search = np.zeros((len(p_list), len(q_list)))
+    for p in p_list:
+        for q in q_list:
+            for d in d_list:
+                try:
+                    my_arima = run_arima(X, train_size, test_size, p, d, q)
+                    my_arima.run()
+                    my_arima.plot_arima()
+                    print(p, d, q, my_arima.mse)
+                except Exception as e:
+                    print(p, d, q, "\n", e)
+
+    plt.figure(10)
+    plt.plot(X[train_size:train_size + test_size], label="Data")
+    plt.legend()
+    # print(grid_search)
 
 
 class run_arima(object):
     """Run ARIMA model."""
 
-    def __init__(self, X, train_size, test_size, p_list, d, q):
+    def __init__(self, X, train_size, test_size, p, d, q):
         """Initialise parameters."""
         self.X = X
         self.train_size = train_size
         self.test_size = test_size
-        self.p_list = p_list
-        self.predictions = np.zeros((len(p_list), test_size))
+        # self.p_list = p_list
+        self.predictions = []  # np.zeros(test_size)
+        self.p = p
         self.d = d
         self.q = q
         self.train = X[0:self.train_size]
@@ -99,39 +115,33 @@ class run_arima(object):
 
     def run(self):
         """Run the model."""
-        for p in self.p_list:
-            history = [t for t in self.train]
-            for t in range(self.test_size):
-                model = ARIMA(history, order=(p,
-                                              self.d,
-                                              self.q))
-                model_fit = model.fit(disp=0, maxiter=500, method='css')
-                output = model_fit.forecast()
-                y_hat = output[0]
-                self.predictions[self.p_list.index(p)][t] = y_hat
+        history = [t for t in self.train]
 
-                obs = self.test[t]
-                history.append(obs)
-                history = history[1:]
+        # Make predictions
+        for t in range(self.test_size):
+            model = ARIMA(history, order=(self.p,
+                                          self.d,
+                                          self.q))
+            model_fit = model.fit(disp=0, maxiter=500, method='css')
+            y_hat = model_fit.forecast()[0]
+            self.predictions.append(y_hat)
+            history.append(self.test[t])
+            # history = history[1:]
+
+        self.mae = mean_absolute_error(self.test, self.predictions)
+        self.mse = mean_squared_error(self.test, self.predictions)
+        self.r2_score = r2_score(self.test, self.predictions)
 
     def plot_arima(self):
         """Plot results."""
-        plt.figure()
-        for p in self.p_list:
-            print('MAE with p =', p, 'is:',
-                  mean_absolute_error(self.test,
-                                      self.predictions[self.p_list.index(p)]))
-            print('MSE with p =', p, 'is:',
-                  mean_squared_error(self.test,
-                                     self.predictions[self.p_list.index(p)]))
-            print('MSE with p =', p, 'is:',
-                  r2_score(self.test,
-                           self.predictions[self.p_list.index(p)]))
+        plt.figure(10)
+        # print('MAE with p =', self.p, 'is:', self.mae)
+        # print('MSE with p =', self.p, 'is:', self.mse)
+        # print('R2 with p =', self.p, 'is:', self.r2_score)
 
-            plt.plot(self.predictions[self.p_list.index(p)],
-                     label='p = ' + str(p))
+        plt.plot(self.predictions, label="p = %s; q = %s; d = %s"
+                 % (str(self.p), str(self.q), str(self.d)))
         plt.grid(which='both')
-        plt.legend()
 
 
 def read_csv(filename):
