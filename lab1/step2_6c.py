7eae8a1174061c0eb527e33f9e71837bb521b890
Ninja <s223012@studenti.polito.it>
2020-02-21 06:19:59 +0100

    update

--- lab1/step2_6c.py
+++ lab1/step2_6c.py
@@ -7,6 +7,7 @@ and ending in area j. Try to visualize the results in a meaningful way.
 """
 import pymongo as pm
 import pprint
+import json
 
 COLLECTION = 'PermanentBookings'
 CITY = "Torino"
@@ -28,61 +29,83 @@ def main():
     """Define main function."""
     # Torino 45 - 7.576 , 45 - 7.78 , 45.142 - 7.576 , 45.142 - 7.78
     # 0.004495 verso nord e 0.006358 verso est
-    dx = 0.006358
-    dy = 0.004495
+    dx = 0.006358 * 2 * 8
+    dy = 0.004495 * 2 * 8
     x = 7.576
     y = 45
-    density = [[[[0] * 32 for i in range(32)]* 32 for j in range(32)]* 32 for k in range(32)]
+    n = 2
+    density = [[[[0] * n for i in range(n)]* n for j in range(n)]* n for k in range(n)]
     collection = get_collection()
-    START_HOUR = 18
-    END_HOUR = 24
 
-    for i in range(32):
-        for j in range(32):
-            for k in range(32):
-                for w in range(32):
-                    coursor = collection.aggregate(
-                        [
-                            {'$match': {'city': CITY}},
-                            {'$project':
-                                {'_id': 0,
-                                 'origin': {'$arrayElemAt':
-                                            ["$origin_destination.coordinates",
-                                             0]},
-                                 'destination':
-                                     {'$arrayElemAt':
-                                         ["$origin_destination.coordinates",
-                                          1]},
-                                 'hour_of_day': {'$hour': '$init_date'}
-                                 }},
-                            {'$match': {'hour_of_day': {'$gte': START_HOUR,
-                                                        '$lt': END_HOUR}}},
-                            {'$project': {'_id': 0,
-                                          'origin_x': {'$arrayElemAt':
-                                                       ["$origin", 0]},
-                                          'origin_y': {'$arrayElemAt':
-                                                       ["$origin", 0]},
-                                          'destination_x': {'$arrayElemAt':
-                                                            ["$destination",
-                                                             0]},
-                                          'destination_y': {'$arrayElemAt':
-                                                            ["$destination",
-                                                             0]}
-                                          }},
-                            {'$match': {'origin_x': {'$gte': x + j * dx,
-                                                     '$lt': x + (j+1) * dx},
-                                        'origin_y': {'$gte': y + i * dy,
-                                                     '$lt': y + (i+1) * dy},
-                                        'destination_x': {'$gte': x + w * dx,
-                                                          '$lt': x + (w+1)*dx},
-                                        'destination_y': {'$gte': y + k * dy,
-                                                          '$lt': y + (k+1)*dy}}
-                             },
-                            {"$group": {'_id': 0, 'count': {'$sum': 1}}}
-                        ])
-                    density[i][j][k][w] = len(list(coursor))
+    fascie_orarie = [0,6,12,18]
 
-    print("finished")
+    for hour in fascie_orarie:
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    for w in range(n):
+                        coursor = collection.aggregate(
+                            [
+                                {'$match': {'city': CITY}},
+                                {'$project':
+                                    {'_id': 0,
+                                     'origin': {'$arrayElemAt':
+                                                ["$origin_destination.coordinates",
+                                                 0]},
+                                     'destination':
+                                         {'$arrayElemAt':
+                                             ["$origin_destination.coordinates",
+                                              1]},
+                                     'hour_of_day': {'$hour': '$init_date'}
+                                     }},
+                                {'$match': {'hour_of_day': {'$gte': hour,
+                                                            '$lt': hour+6}}},
+                                {'$project': {'_id': 0,
+                                              'origin_x': {'$arrayElemAt':
+                                                           ["$origin", 0]},
+                                              'origin_y': {'$arrayElemAt':
+                                                           ["$origin", 0]},
+                                              'destination_x': {'$arrayElemAt':
+                                                                ["$destination",
+                                                                 0]},
+                                              'destination_y': {'$arrayElemAt':
+                                                                ["$destination",
+                                                                 0]}
+                                              }},
+                                {'$match': {'origin_x': {'$gte': x + j * dx,
+                                                         '$lt': x + (j+1) * dx},
+                                            'origin_y': {'$gte': y + i * dy,
+                                                         '$lt': y + (i+1) * dy},
+                                            'destination_x': {'$gte': x + w * dx,
+                                                              '$lt': x + (w+1)*dx},
+                                            'destination_y': {'$gte': y + k * dy,
+                                                              '$lt': y + (k+1)*dy}}
+                                 },
+                                {"$group": {'_id': 0, 'count': {'$sum': 1}}}
+                            ])
+                        density[i][j][k][w] = len(list(coursor))
+
+        data = []
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    for w in range(n):
+                        if density[i][j][k][w] > 5:
+                            d = {
+                                "type": "Feature",
+                                "properties": {"load": density[i][j][k][w]},
+                                "geometry": {
+                                    "type": "LineString",
+                                    "coordinates": [
+                                        [x+(i*dx), y+(j*dy)],
+                                        [x+(k*dx), y+(w*dy)]
+                                    ]
+                                }
+                            }
+                            data.append(d)
+        filename = "data_"+str(hour)+".json"
+        with open(filename, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
 
 if __name__ == '__main__':
     main()
